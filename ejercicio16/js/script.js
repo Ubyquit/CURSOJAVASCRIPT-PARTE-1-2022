fd017b6f03b2f535b30a0334ec8f522776b60457
ubyquit <ingsystem.out.print@gmail.com>
2022-09-28 21:42:47 -0500

    Ejercicio 16

---
+++ ejercicio16/js/script.js
@@ -0,0 +1,13 @@
+let x = 10;
+let y = 14;
+
+/* Bloque de codigo { } */
+if (x > y) {
+  document.write("X es mayor Y"); // Linea de codigo ;
+} else if (x < y) {
+  document.write("Y es mayor a X");
+} else if (x == y) {
+  document.write(" X e Y son iguales");
+} else {
+  document.write("X no es mayor a Y");
+}
